8f2fbf6f22462d21137a85252c4c6bacfe0e6279
abdellahabd <99611751+abdellahabd@users.noreply.github.com>
2024-07-04 17:33:58 +0100

    chore: Update Docker Compose configuration for development database

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authservice: AuthService) {}
   @Post('singup')
-  singup() {
-    return this.authservice.singup();
+  singup(@Body() dto: AuthDto) {
+    return this.authservice.singup(dto);
   }
   @Post('singin')
-  singin() {
-    return this.authservice.singin();
+  singin(@Body() dto: AuthDto) {
+    return this.authservice.singin(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon2 from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
-  singup() {
-    return { msg: 'i m signed up ' };
+  constructor(private prisma: PrismaService) {}
+  async singup(dto: AuthDto) {
+    try {
+      const hash = await argon2.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken ');
+        }
+      }
+      throw error;
+    }
   }
-  singin() {
-    return { msg: 'i m signed in' };
+  async singin(dto: AuthDto) {
+    const getUser = await this.prisma.user.findFirst({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!getUser) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    const pwmatch = await argon2.verify(getUser.hash, dto.password);
+    if (!pwmatch) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    delete getUser.hash;
+    return getUser;
   }
 }

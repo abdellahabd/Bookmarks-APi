0614de0516826c66c727da4d2ced78090b22ed11
abdellahabd <99611751+abdellahabd@users.noreply.github.com>
2024-07-12 20:01:04 +0100

    chore: Update TypeScript and ESLint configuration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authservice: AuthService) {}
+  @HttpCode(HttpStatus.CREATED)
   @Post('singup')
   singup(@Body() dto: AuthDto) {
     return this.authservice.singup(dto);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '@prisma/client';
 
-import { GetUser } from 'src/auth/decorators/index';
+import { GetUser } from '../auth/decorators/get-user';
 
 @Controller('users')
 export class UserController {

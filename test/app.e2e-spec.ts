6f6bfe87490aa7f64aaf058a06e6af198c97ba19
abdellahabd <99611751+abdellahabd@users.noreply.github.com>
2024-07-13 18:46:07 +0100

    chore: Add e2e tests for signup and signin endpoints

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,8 @@ import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from '../src/prisma/prisma.service';
+import * as pactum from 'pactum';
+import { AuthDto } from '../src/auth/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -14,8 +16,10 @@ describe('App e2e', () => {
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+    app.listen(3334);
     prisma = app.get(PrismaService);
-    prisma.cleanDb();
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3334/');
   });
 
   afterAll(() => {
@@ -23,10 +27,51 @@ describe('App e2e', () => {
   });
 
   describe('Auth', () => {
-    describe('Singin', () => {});
-    describe('Singup', () => {});
-    describe('Auth', () => {});
+    const dto: AuthDto = {
+      email: 'abdellahouazane@2002.com',
+      password: 'abdellah2002',
+    };
+    describe('Singup', () => {
+      it('should singup', () => {
+        return pactum
+          .spec()
+          .post('auth/singup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+      ///dir t3 email erure
+      ///dir t3 password erure erure
+    });
+    describe('Singin', () => {
+      it('should singup', () => {
+        return pactum
+          .spec()
+          .post('auth/singin')
+          .withBody(dto)
+          .expectStatus(201)
+          .stores('userAt', 'access_token');
+      });
+      ///dir t3 email erure
+      ///dir t3 password erure erure
+    });
   });
-  describe('User', () => {});
-  describe('BookMarks', () => {});
+  describe('User', () => {
+    describe('Get me', () => {
+      it('should get current user', () => {
+        return pactum
+          .spec()
+          .get('users/me')
+          .withHeaders({ Authorization: 'Bearer $S{userAt}' })
+          .expectStatus(200);
+      });
+    });
+    describe('Edit user', () => {});
+  });
+  describe('BookMarks', () => {
+    describe('create bookmark', () => {});
+    describe('get bookmarks', () => {});
+    describe('get bookmark by id', () => {});
+    describe('edit bookmark', () => {});
+    describe('delete bookmark', () => {});
+  });
 });
